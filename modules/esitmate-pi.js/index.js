e239de92ee7a58de3c86d1c8d8f879883114f41a
Michael Wells <mwells1286@gmail.com>
2021-02-27 08:57:58 -0800

    add solution to pin problem

--- modules/esitmate-pi.js/index.js
+++ modules/esitmate-pi.js/index.js
@@ -1,6 +1,17 @@
 const estimatePi = (n) => {
-  let result = 0;
+  let pointsInCircle = 0,
+    pointsTotal = 0;
 
+  for (let i = 0; i < n; i++) {
+    let x = Math.random();
+    let y = Math.random();
+    let distance = x ** 2 + y ** 2;
+    if (distance < 1) {
+      pointsInCircle += 1;
+    }
+    pointsTotal += 1;
+  }
+  let result = 4 * (pointsInCircle / pointsTotal);
   return result;
 };
 
--- modules/esitmate-pi.js/test.js
+++ modules/esitmate-pi.js/test.js
@@ -1,5 +1,9 @@
 const estimatePi = require("./index");
 
 test("it returns a result", () => {
-  expect(estimatePi()).toBeDefined();
+  expect(estimatePi(1)).toBeDefined();
+});
+
+test("it returns an estimate of pi based on n data points between 1 and 0 ", () => {
+  expect(estimatePi(100000)).toBeCloseTo(3.14, 1);
 });
