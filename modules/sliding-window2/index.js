3c9ad809dba2be3befd8cb1ddcb4eda08c29d69f
Michael Wells <mwells1286@gmail.com>
2021-03-06 13:13:11 -0800

    add initial solution

--- modules/sliding-window2/index.js
+++ modules/sliding-window2/index.js
@@ -1,13 +1,19 @@
 const longest_substring_with_k_distinct = function (str, k) {
-  let windowStart = 0,
-    maxLength = 0,
-    charFrequency = {};
-
-  for (let i = 0; i < k; i++) {
-    let rightChar = str[i];
-    if (!(rightChar in charFrequency)) {
-      charFrequency[rightChar] = 0;
+  let longest = 0;
+  while (str.length >= 1) {
+    let charFrequency = {},
+      tempLongest = 0;
+    for (let i = 0; i < str.length; i++) {
+      let rightChar = str[i];
+      if (!(rightChar in charFrequency)) {
+        charFrequency[rightChar] = 0;
+      }
+      if (Object.keys(charFrequency).length > k) break;
+      charFrequency[rightChar] += 1;
     }
-    charFrequency[rightChar] += 1;
+    str = str.slice(1);
+    tempLongest = Object.values(charFrequency).reduce((a, b) => a + b);
+    if (tempLongest > longest) longest = tempLongest;
   }
+  return longest;
 };
