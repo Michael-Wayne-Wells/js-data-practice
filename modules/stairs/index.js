30e9faf404b80fea10cdde6dc37cd8c758ac6232
Michael Wells <mwells1286@gmail.com>
2021-03-07 11:04:50 -0800

    add memory save for array

--- modules/stairs/index.js
+++ modules/stairs/index.js
@@ -4,9 +4,13 @@ function stairCount(n) {
   // start loop with 1 and 0 accounted for
   for (let i = 1; i < n; i++) {
     let count = countArray[1] + countArray[0];
+    // only need last two numbers
+    countArray.splice(-1, 1);
+
     countArray.unshift(count);
     // last number counted will be the total possible. unshift and return first count.
   }
   return countArray[0];
 }
+// stairCount(100);
 export default stairCount;
