e0e49db0cfd3b0def88860548f6b4255f4d513d4
Michael Wells <mwells1286@gmail.com>
2021-03-12 11:03:37 -0800

    refactor

--- modules/sliding-window3/index.js
+++ modules/sliding-window3/index.js
@@ -12,10 +12,9 @@ export default function fruits_into_baskets(fruits) {
         if (Object.keys(fruitTree).length == 2) break;
         fruitTree[fruit] = 0;
       }
-      fruitTree[fruit] += 1;
     }
-    let newCount = Object.values(fruitTree).reduce((t, v) => t + v);
-    if (newCount > currentHigh) currentHigh = newCount;
+    let newCount = end - start;
+    currentHigh = Math.max(currentHigh, newCount);
     start += 1;
   }
   return currentHigh;
--- modules/sliding-window3/test.js
+++ modules/sliding-window3/test.js
@@ -1,5 +1,8 @@
 import fruits_into_baskets from "./index";
 
+test("it returns the maximum number of two unique fruits in both baskets 3.", () => {
+  expect(fruits_into_baskets(["A", "B", "C", "A", "C"])).toEqual(3);
+});
 test("it returns the maximum number of two unique fruits in both baskets.", () => {
   expect(fruits_into_baskets(["A", "B", "C", "B", "B", "C"])).toEqual(5);
 });
