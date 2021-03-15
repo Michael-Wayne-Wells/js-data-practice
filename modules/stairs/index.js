d253d0f6ea5d748d2bbb2109d27e800e89c48281
Michael Wells <mwells1286@gmail.com>
2021-03-15 08:19:37 -0700

    update package structure

--- modules/stairs/index.js
+++ modules/stairs/index.js
@@ -1,16 +1,6 @@
-function stairCount(n) {
-  let countArray = [1, 1];
-  // if n is 0 or 1, answer will always be 1.
-  // start loop with 1 and 0 accounted for
-  for (let i = 1; i < n; i++) {
-    let count = countArray[1] + countArray[0];
-    // only need last two numbers
-    countArray.splice(-1, 1);
-
-    countArray.unshift(count);
-    // last number counted will be the total possible. unshift and return first count.
-  }
-  return countArray[0];
-}
-// stairCount(100);
+import stairCount from "./stairCount.js";
+import stairCountRecursive from "./stairCountRecursive.js";
+// console.log(stairCountRecursive(100));
+// console.log(stairCount(100));
 export default stairCount;
+export default stairCountRecursive;
